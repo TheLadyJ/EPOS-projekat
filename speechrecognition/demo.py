0655301db5f8c8986af078615dc867b5fad19114
TheLadyJ <jovana.vida@gmail.com>
2021-01-27 22:24:12 +0100

    2nd code

--- speechrecognition/demo.py
+++ speechrecognition/demo.py
@@ -8,19 +8,42 @@ r3 = sr.Recognizer()
 with sr.Microphone() as source:
     print('[search edureka: search youtube]')
     print('speak now')
-    audio = r3.listen(source)
+    r3.adjust_for_ambient_noise(source, duration=1)
+    audio = r3.listen(source, timeout=4)
 
-if 'edureka' in r2.recognize_google(audio):
+
+if 'video' in r1.recognize_google(audio):
+    r1 = sr.Recognizer()
+
+    url = 'https://www.youtube.com/results?search_query='
+    with sr.Microphone() as source:
+        print('search your query')
+        r1.adjust_for_ambient_noise(source, duration=1)
+        audio = r1.listen(source, timeout=4)
+       
+        try:
+            get = r1.recognize_google(audio)
+            print(get)
+            wb.get().open_new_tab(url+get)
+        except sr.UnknownValueError:
+            print('error')
+        except sr.RequestError as e:
+            print('failed'.format(e))
+    
+
+if '2' in r2.recognize_google(audio):
     r2 = sr.Recognizer()
+
     url = 'https://www.edureka.co/'
     with sr.Microphone() as source:
         print('search your query')
-        audio = r2.listen(source)
-
+        r2.adjust_for_ambient_noise(source, duration=1)
+        audio = r2.listen(source, timeout=4)
+       
         try:
             get = r2.recognize_google(audio)
             print(get)
-            wb.get().open_new(url+get)
+            wb.get().open_new_tab(url+get)
         except sr.UnknownValueError:
             print('error')
         except sr.RequestError as e:
